77030e8536fa52f034fd836a50993faf859cd6a1
Angel Georgiev <angellbgpl@gmail.com>
2021-04-09 00:09:34 +0300

    second

--- script.py
+++ script.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sys
 import ForeignWordsAnalytics.venv.main
 
@@ -7,14 +6,4 @@ text = ' Най-ранните свидетелства за човешко пр
 text = [ord(c) for c in text]
 print(text)
 
-=======
-import sys
-import ForeignWordsAnalytics.venv.main
-
-# print('Hello from python')
-text = ' Най-ранните свидетелства за човешко присъствие по земите на днешна България датират от преди 200 – 100 хиляди години.'
-text = [ord(c) for c in text]
-print(text)
-
->>>>>>> 425d20905ad95358dbe01f6f2215d5cf8f1ef916
 # exec()
